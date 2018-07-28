6ec553cd6e2acad58186ec7a526ab445959d5106
Akshay <akshay.mittal@slalom.com>
2018-07-28 16:33:53 -0500

    updating the resources

--- safety-net/lib/logic.js
+++ safety-net/lib/logic.js
@@ -1,10 +1,10 @@
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.example.mynetwork.Trade} trade - the trade to be processed
+ * @param {dev.Trade} trade - the trade to be processed
  * @transaction
  */
 async function tradeCommodity(trade) {
     trade.commodity.owner = trade.newOwner;
-    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
+    let assetRegistry = await getAssetRegistry('dev.Commodity');
     await assetRegistry.update(trade.commodity);
 }
