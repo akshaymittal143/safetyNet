a6082fa9b8462df27f09bace6a76488666293893
Akshay <akshay.mittal@slalom.com>
2018-07-28 16:29:43 -0500

    updated the acl

--- safety-net/lib/logic.js
+++ safety-net/lib/logic.js
@@ -7,4 +7,4 @@ async function tradeCommodity(trade) {
     trade.commodity.owner = trade.newOwner;
     let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
     await assetRegistry.update(trade.commodity);
-}git
+}
